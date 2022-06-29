de2d824e4fc71c28ac6810df8e5c71576ecc9207
andynic <andysnic@hotmail.com>
2022-06-29 13:33:33 +0100

    Initial Commit

---
+++ fazan/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  fazan
+//
+//  Created by Andy Nic on 29/06/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
