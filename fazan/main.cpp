b78e5f2923e6940f55fa0e1c1e8ae32b6775297a
andynic <andysnic@hotmail.com>
2022-07-01 14:53:00 +0100

    fixed file path run function

--- fazan/main.cpp
+++ fazan/main.cpp
@@ -14,7 +14,9 @@ bool are_words_linked(string word1, string word2)
 
 void add_used_word(string word)
 {
-    
+    ofstream used_words("used_words.txt");
+    used_words<<word<<'\n';
+    used_words.close();
 }
 
 vector<string> get_used_words()
@@ -38,6 +40,17 @@ bool is_used(string word)
 vector<string> get_all_words()
 {
     vector<string> all_words = {"test"};
+    
+    ifstream dictionary;
+    dictionary.open("/Users/andynic/c++ projects/fazan/fazan/list.txt");
+    string content;
+    if ( dictionary.is_open() )
+    {
+        dictionary >> content;
+        cout << content;
+    }
+    dictionary.close();
+    
     return all_words;
 }
 
@@ -60,7 +73,8 @@ void display_response(string word)
 
 void display_turnover(int score1, int score2)
 {
-    
+    cout << "First player score is " << score1 << endl;
+    cout << "Second player score is " << score2 << endl;
 }
 
 string bot1(string last_letters)
@@ -73,8 +87,45 @@ string bot2(string last_letters)
     return 0;
 }
 
+void setup_game(int &first_player_score, int &second_player_score, string &last_word, string &second_to_last_word)
+{
+    
+}
+
+bool is_game_over(int first_player_score, int second_player_score)
+{
+    return 1;
+}
+
+bool is_round_over(string last_word, string second_to_last_word)
+{
+    return 1;
+}
+
+void run_game()
+{
+    int first_player_score;
+    int second_player_score;
+    string last_word;
+    string second_to_last_word;
+    bool is_player_one_starting = rand() % 2;
+    bool is_player_one_turn = is_player_one_starting;
+
+    setup_game(first_player_score, second_player_score, last_word, second_to_last_word);
+    
+    while(!is_game_over(first_player_score, second_player_score))
+    {
+        while(!is_round_over(last_word, second_to_last_word))
+        {
+            
+        }
+        is_player_one_turn = !is_player_one_turn;
+    }
+}
+
 int main()
 {
+    get_all_words();
     return 0;
 }
 
