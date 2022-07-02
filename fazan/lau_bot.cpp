c414be7016f59ea664a25822a1df778e559785dc
alex <alexnic011011@gmail.com>
2022-07-03 00:30:56 +0100

    uncomment

--- fazan/lau_bot.cpp
+++ fazan/lau_bot.cpp
@@ -17,11 +17,11 @@ string lau_bot::get_reply(string last_word)
     if(last_word.length() >= 2)     // Valid word
     {
         auto ending = last_word.substr(last_word.length() - 2);
-//        vector<string> words = Database::get_all_words();
-//        auto it = find_if(words.begin(), words.end(), [&](const auto& x) { return x.starts_with(ending);});
-//        if(it != words.end()) {
-//            return *it;
-//        }
+        vector<string> words = database::get_all_words();
+        auto it = find_if(words.begin(), words.end(), [&](const auto& x) { return x.starts_with(ending);});
+        if(it != words.end()) {
+            return *it;
+        }
     }
     
     return "";
